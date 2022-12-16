ed7f2548f05564abf2e2e82b3f376352a53a8946
tobias-mayr <tobias.mayr@alphasights.com>
2022-12-16 19:09:32 +0000

    add heat index display

--- src/main/java/org/designpatterns/c02observerpattern/WeatherORama.java
+++ src/main/java/org/designpatterns/c02observerpattern/WeatherORama.java
@@ -3,6 +3,7 @@ package org.designpatterns.c02observerpattern;
 
 import org.designpatterns.c02observerpattern.observer.CurrentConditionsDisplay;
 import org.designpatterns.c02observerpattern.observer.ForecastDisplay;
+import org.designpatterns.c02observerpattern.observer.HeatIndexDisplay;
 import org.designpatterns.c02observerpattern.observer.StatisticsDisplay;
 import org.designpatterns.c02observerpattern.subject.WeatherData;
 
@@ -13,6 +14,7 @@ public class WeatherORama {
         CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
         StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
         ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+        HeatIndexDisplay heatIndexDisplay = new HeatIndexDisplay(weatherData);
 
         weatherData.setMeasurements(30, 65, 23.3f);
         weatherData.setMeasurements(30, 20, 30.2f);
